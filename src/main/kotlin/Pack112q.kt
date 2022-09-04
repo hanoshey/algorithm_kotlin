e2403ea84c184c14c3bcca475b33dffdc3a94785
zynastor <zynastorinc@gmail.com>
2022-09-04 18:54:36 +0900

    315p

---
+++ src/main/kotlin/Pack112q.kt
@@ -0,0 +1,19 @@
+import java.util.Scanner
+
+object Pack112q {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val sc = Scanner(System.`in`)
+        val str = sc.next()
+        //첫 번째 문자를 숫자로 변경한 값을 대입
+        var result = (str[0] - '0').toLong()
+        for (i in 1 until str.length) {
+            //두 수 중에서 하나라도 '0' 또는 '1'일 경우,
+            //곱하기보다는 더하기 수행
+            val num = str[i] - '0'
+            if (num <= 1 || result <= 1) result += num
+            else result *= num
+        }
+        println(result)
+    }
+}
---
+++ src/main/kotlin/Pack113q.kt
@@ -0,0 +1,26 @@
+import java.util.*
+import kotlin.math.min
+
+object Pack113q {
+    var str = ""
+    var count0 = 0//전부 0으로 바꾸는 경우
+    var count1 = 0//전부 1로 바꾸는 경우
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val sc = Scanner(System.`in`)
+        str = sc.next()
+        //첫 번째 원소에 대해 처리
+        if (str[0] == '1') count0 += 1
+        else count1 += 1
+        //두 번째 원소부터 모든 원소를 확인하며
+        for (i in 0 until str.length - 1) {
+            if (str[i] != str[i + 1]) {
+                //다음 수에서 1로 바뀌는 경우
+                if (str[i + 1] == '1') count0 += 1
+                else count1 += 1
+            }
+        }
+        println(min(count0, count1))
+    }
+}
